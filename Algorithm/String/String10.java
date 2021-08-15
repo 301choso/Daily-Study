25bf2d93ad5cd109f633449513b99ec85971a760
SoHyun Cho <78252188+301choso@users.noreply.github.com>
2021-08-15 14:51:40 +0900

    Update String10.java

--- Algorithm/String/String10.java
+++ Algorithm/String/String10.java
@@ -1,6 +1,5 @@
 package com.tj;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class String10 {
@@ -8,18 +7,35 @@ public class String10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str =sc.next();
-		String word =sc.next();
+		char word =sc.next().charAt(0);
 		int[] list = new int[str.length()];
-		ArrayList<Integer> index = new ArrayList<Integer>(); 
+		int p=1000;
 		for(int i=0;i<str.length();i++) {
-			if(str.charAt(i)==word.charAt(0)) {
-				list[i]=0;
-				index.add(i);
+			if(str.charAt(i)==word) {
+				p=0;
+				list[i]=p;
+				
+			}else {
+				p++;
+				list[i]=p;
+			}
+	
+		}
+		
+		p=1000;
+		for(int i=str.length()-1;i>=0;i--) {
+			if(str.charAt(i)==word) {
+				p=0;
+				
+			}else {
+				p++;
+				list[i]=Math.min(list[i],p);
 			}
 		}
 		
-		
+		for(int k=0;k<list.length;k++) {
+			System.out.print(list[k]+" ");
+		}
 		
 	}
-
 }
