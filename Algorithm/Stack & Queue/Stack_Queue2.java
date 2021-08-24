4de4369a9cc7b3cdfcb7f44a52d4a746826093d9
SoHyun Cho <78252188+301choso@users.noreply.github.com>
2021-08-24 16:39:07 +0900

    Add files via upload

---
+++ Algorithm/Stack & Queue/Stack_Queue2.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack_Queue2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int cnt=0;
+		Stack<Character> stack = new Stack<>();
+		StringBuilder st = new StringBuilder();
+		for(Character i : str.toCharArray()) {
+			if(i=='(') {
+				stack.push(i);
+				cnt++;
+			}else if(i==')') {
+				if(cnt>=1) {
+					cnt--;
+					stack.pop();
+				}
+			}else {
+				if(cnt==0) {
+					st.append(i);
+				}
+			}
+		}
+		System.out.println(st.toString());
+		
+	}
+}
