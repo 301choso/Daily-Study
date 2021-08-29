c61ae695f3ed559ba616754a4c4f77778e3ed382
SoHyun Cho <78252188+301choso@users.noreply.github.com>
2021-08-30 00:32:37 +0900

    Add files via upload

---
+++ Algorithm/Stack & Queue/Stack_Queue5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack_Queue5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		Stack<Character> stack = new Stack<>();
+		
+		int cnt=0;
+		for(int i=0;i<str.length();i++){
+			
+			if(str.charAt(i)=='(') {
+				stack.push(str.charAt(i));				
+			}else {
+				stack.pop();
+				if(str.charAt(i-1)=='(') {		
+					cnt+=stack.size();	
+				}else {
+					cnt++;
+				}	
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+}
